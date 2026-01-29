2733022af39e2b9bdbcd718c2be2f0e3ae55469f
jldavid <jldavid019@gmail.com>
2026-01-29 15:16:35 +0800

    model: modifed price and created_at fields

--- src/main/java/com/david/readme/models/Book.java
+++ src/main/java/com/david/readme/models/Book.java
@@ -11,7 +11,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 
 @Entity
@@ -22,12 +23,13 @@ import java.time.LocalDate;
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @Column(name = "id")
+    private Long id;
 
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "description")
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
     private String description;
 
     @Column(name = "author", nullable = true)
@@ -36,15 +38,20 @@ public class Book {
     @Column(name = "slug", unique = true, nullable = false)
     private String slug;
 
-    @Column(name = "price", nullable = false)
-    private double price;
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
 
     @Column(name = "stock", nullable = false)
     private int stock;
 
     @Column(name = "published_at", nullable = false)
-    private LocalDate publishAt;
+    private LocalDateTime publishAt;
 
-    @Column(name = "created_at", nullable = false)
-    private LocalDate createdAt;
+    @Column(
+        name = "created_at",
+        insertable = false,
+        updatable = false,
+        columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+    )
+    private LocalDateTime createdAt;
 }
