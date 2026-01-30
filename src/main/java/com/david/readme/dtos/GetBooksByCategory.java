5d588a7cc9e0514d48a2b6412a260dfeda21eeb9
jldavid <jldavid019@gmail.com>
2026-01-30 16:04:39 +0800

    dto: added DTO for findBooksByCategory query

---
+++ src/main/java/com/david/readme/dtos/GetBooksByCategory.java
@@ -0,0 +1,14 @@
+package com.david.readme.dtos;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record GetBooksByCategory(
+        String title,
+        String author,
+        String description,
+        BigDecimal price,
+        int stock,
+        String slug,
+        LocalDateTime publishedAt
+) {}
