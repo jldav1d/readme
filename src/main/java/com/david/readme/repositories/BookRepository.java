8bcce4b42f912fd161d88de3b8894805df5805b3
jldavid <jldavid019@gmail.com>
2026-01-30 16:03:40 +0800

    repository: added findBooksByCategory in BookRepository

--- src/main/java/com/david/readme/repositories/BookRepository.java
+++ src/main/java/com/david/readme/repositories/BookRepository.java
@@ -1,8 +1,22 @@
 package com.david.readme.repositories;
 
+import com.david.readme.dtos.GetBooksByCategory;
 import com.david.readme.models.Book;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface BookRepository extends ListCrudRepository<Book, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByTitle(String title);
+    Optional<Book> findBySlug(String slug);
+
+
+    @Query("SELECT new com.david.readme.dtos.GetBooksByCategory(b.title, b.author, b.description, b.price, b.stock, b.slug, b.publishedAt)" +
+            "FROM Book b JOIN b.categories c " +
+            "WHERE LOWER(c.name) = :categoryName"
+    )
+    List<GetBooksByCategory> findBooksByCategory(@Param("categoryName") String categoryName);
 }
