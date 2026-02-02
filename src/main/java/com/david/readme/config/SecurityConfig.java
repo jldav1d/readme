5e5e455162a93efc763336a5c189e2d125aab4a2
jldavid <jldavid019@gmail.com>
2026-02-02 14:30:40 +0800

    config: created SecurityConfig for handling authentication

---
+++ src/main/java/com/david/readme/config/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.david.readme.config;
+
+import com.david.readme.services.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(this.userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+
+                    // public endpoints
+                    .requestMatchers("/", "/index.html", "/books.html","/login.html", "/register.html").permitAll()
+                    .requestMatchers("/css/**", "/js/**").permitAll()
+                    .requestMatchers("/api/v1/auth/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/v1/books/**").permitAll()
+
+                    // protected endpoints
+                    .requestMatchers("/cart.html", "/orders.html").authenticated()
+                    .requestMatchers("/api/v1/cart/**", "/api/v1/orders/**").authenticated()
+
+                    // authenticate any other request by default
+                    .anyRequest().authenticated()
+            )
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .logout(logout -> logout
+                    .logoutUrl("/api/v1/auth/logout")
+                    .logoutSuccessHandler(logoutSuccessHandler())
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+            );
+
+        return http.build();
+    }
+
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+            return (request, response, authentication) -> {
+                response.setStatus(200);
+                response.getWriter().write("You have been logged out successfully.");
+                response.setContentType("application/json");
+            };
+    }
+
+}
