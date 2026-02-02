6579ddec498e81bb3b47a37618adab036ea4ad76
jldavid <jldavid019@gmail.com>
2026-02-02 15:35:55 +0800

    frontend: added registerUser function for register form submission

---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,45 @@
+const LOGIN_API_URL = 'http://localhost:8080/api/v1/auth/register';
+const registerForm = document.querySelector('#register-form');
+const errorText = document.querySelector('#error-text');
+
+registerForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    await registerUser();
+})
+
+async function registerUser(){
+
+    errorText.classList.add('hidden');
+    errorText.textContent = '';
+
+    const formData = new FormData(registerForm);
+
+    try{
+        const response = await fetch(LOGIN_API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: formData.get('username'),
+                password: formData.get('password')
+            })
+        })
+
+        if (!response.ok) {
+            const errorData = await response.json().catch(() => ({}));
+            throw new Error(errorData.message || 'Invalid username or password');
+        }
+
+
+        console.log("Registered successfully!");
+
+        // redirect user to index.html
+        window.location.href = 'index.html';
+    }
+    catch(error){
+        errorText.classList.remove('hidden');
+        errorText.textContent = error.message;
+        console.log(error);
+    }
+}
