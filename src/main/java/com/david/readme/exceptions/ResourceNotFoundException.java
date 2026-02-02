1021d2337fa0f77e793d89fa51ba8c5c6871fbf2
jldavid <jldavid019@gmail.com>
2026-02-02 09:52:13 +0800

    exceptions: added ResourceNotFound & UnauthorizeException classes

---
+++ src/main/java/com/david/readme/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.david.readme.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/david/readme/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.david.readme.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
